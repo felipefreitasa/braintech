ce8ce800cd8f8147c56cd5722558e4d355df5ba3
Felipe <felipe.freitas@somapay.com.br>
2023-07-21 08:55:46 -0300

    feat: adding entering animations at welcome screen

--- src/screens/Welcome/index.tsx
+++ src/screens/Welcome/index.tsx
@@ -1,12 +1,13 @@
-import { TouchableOpacity } from "react-native"
-import { useNavigation } from "@react-navigation/native"
+import { TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Animated, { FadeIn } from "react-native-reanimated";
 
-import { AuthNavigatorRoutesProps } from "../../routes/auth.routes"
+import { AuthNavigatorRoutesProps } from "../../routes/auth.routes";
 
-import LogoSvg from "@assets/logo.svg"
-import SignInBackgroundImg from "@assets/sign-in-background.png"
+import LogoSvg from "@assets/logo.svg";
+import SignInBackgroundImg from "@assets/sign-in-background.png";
 
-import { Button } from "@components/Button"
+import { Button } from "@components/Button";
 
 import {
   Title,
@@ -17,47 +18,53 @@ import {
   TitleDescription,
   SignInButtonLabel,
   SignInButtonLabelHighlight,
-} from "./styles"
+} from "./styles";
 
 export function Welcome() {
-  const { navigate } = useNavigation<AuthNavigatorRoutesProps>()
+  const { navigate } = useNavigation<AuthNavigatorRoutesProps>();
 
   return (
     <Container>
       <Background source={SignInBackgroundImg} resizeMode="cover">
         <Content>
-          <LogoSvg width={54} height={54} />
+          <Animated.View entering={FadeIn.duration(600)}>
+            <LogoSvg width={54} height={54} />
 
-          <Title>
-            BrainTech
-            <TitleDescription>: O quiz de tecnologia</TitleDescription>
-          </Title>
+            <Title>
+              BrainTech
+              <TitleDescription>: O quiz de tecnologia</TitleDescription>
+            </Title>
 
-          <Subtitle>
-            Teste seus conhecimentos e domine frameworks e linguagens de
-            programação!
-          </Subtitle>
+            <Subtitle>
+              Teste seus conhecimentos e domine frameworks e linguagens de
+              programação!
+            </Subtitle>
+          </Animated.View>
 
-          <Button
-            disabled={false}
-            isLoading={false}
-            title="Criar conta"
-            onPress={() => navigate("nameOnboarding")}
-          />
-
-          <TouchableOpacity
-            activeOpacity={0.8}
-            onPress={() => navigate("signIn")}
-          >
-            <SignInButtonLabel>
-              Já possui conta?{" "}
-              <SignInButtonLabelHighlight>
-                Entre agora!
-              </SignInButtonLabelHighlight>
-            </SignInButtonLabel>
-          </TouchableOpacity>
+          <Animated.View entering={FadeIn.delay(300).duration(600)}>
+            <View style={{ height: 46, width: "100%" }}>
+              <Button
+                disabled={false}
+                isLoading={false}
+                title="Criar conta"
+                onPress={() => navigate("nameOnboarding")}
+              />
+            </View>
+            
+            <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={() => navigate("signIn")}
+            >
+              <SignInButtonLabel>
+                Já possui conta?{" "}
+                <SignInButtonLabelHighlight>
+                  Entre agora!
+                </SignInButtonLabelHighlight>
+              </SignInButtonLabel>
+            </TouchableOpacity>
+          </Animated.View>
         </Content>
       </Background>
     </Container>
-  )
+  );
 }
