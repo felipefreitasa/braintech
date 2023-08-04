1af68bff07948ab1d342d7c8ac8f5c1d4c0eef58
Felipe <felipe.freitas@somapay.com.br>
2023-08-04 14:58:09 -0300

    feat: navigating to profile screem when user clicks at profile picture at home

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,5 +1,7 @@
+import { Pressable } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { useTheme } from "styled-components/native";
+import { useNavigation } from "@react-navigation/native";
 import Animated, { FadeIn } from "react-native-reanimated";
 
 import { getSalutation } from "@utils/getSalutation";
@@ -7,6 +9,8 @@ import { getSalutation } from "@utils/getSalutation";
 import { useAuth } from "@hooks/useAuth";
 
 import { UserPhoto } from "@components/UserPhoto";
+
+import { HomeTabsNavigatorRoutesProps } from "../../routes/tabs.routes";
 
 import {
   Salution,
@@ -22,25 +26,32 @@ export function HomeHeader() {
 
   const { COLORS } = useTheme();
 
+  const { navigate } = useNavigation<HomeTabsNavigatorRoutesProps>();
+
   return (
-    <Container>
-      <Animated.View entering={FadeIn}>
-        <LeftContainer>
-          {loggedUser?.user.photoURL ? (
-            <UserPhoto size={40} source={{ uri: loggedUser?.user.photoURL }} />
-          ) : (
-            <ProfileIconContainer>
-              <Feather name="user" size={24} color={COLORS.PRIMARY} />
-            </ProfileIconContainer>
-          )}
+    <Pressable onPress={() => navigate("profile")}>
+      <Container>
+        <Animated.View entering={FadeIn}>
+          <LeftContainer>
+            {loggedUser?.user.photoURL ? (
+              <UserPhoto
+                size={40}
+                source={{ uri: loggedUser?.user.photoURL }}
+              />
+            ) : (
+              <ProfileIconContainer>
+                <Feather name="user" size={24} color={COLORS.PRIMARY} />
+              </ProfileIconContainer>
+            )}
 
-          <TextContent>
-            <Salution>{getSalutation()}</Salution>
+            <TextContent>
+              <Salution>{getSalutation()}</Salution>
 
-            <UserName>{loggedUser?.user.displayName}</UserName>
-          </TextContent>
-        </LeftContainer>
-      </Animated.View>
-    </Container>
+              <UserName>{loggedUser?.user.displayName}</UserName>
+            </TextContent>
+          </LeftContainer>
+        </Animated.View>
+      </Container>
+    </Pressable>
   );
 }
